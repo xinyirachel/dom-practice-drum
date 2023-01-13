31d1973b2725cf57283bd4987141de5d39bbef53
Rachel (Xinyi) Wang <88614537+xinyirachel@users.noreply.github.com>
2023-01-13 17:21:30 +1100

    change style

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
   document
-    .querySelectorAll("button")[i].addEventListener("click", function handleClick() {
-      alert("I got clicked!");
+    .querySelectorAll("button")[i].addEventListener("click",
+    function handleClick() {
+      var audio = new Audio('audio_file.mp3');
+      audio.play();
+
     });
 }
